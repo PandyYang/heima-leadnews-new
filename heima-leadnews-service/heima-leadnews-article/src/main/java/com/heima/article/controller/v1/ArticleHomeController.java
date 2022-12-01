bada10eec50643e4d532abd290d0e50d42441147
PandyYang <yangpandy@gmail.com>
2022-12-01 18:12:29 +0800

    add ArticleFreemarkerTest

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/controller/v1/ArticleHomeController.java
@@ -0,0 +1,28 @@
+package com.heima.article.controller.v1;
+
+import com.heima.app.gateway.model.common.dtos.ResponseResult;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/article")
+public class ArticleHomeController {
+
+    @PostMapping("/load")
+    public ResponseResult load(@RequestBody ArticleHomeDto dto) {
+        return null;
+    }
+
+    @PostMapping("/loadmore")
+    public ResponseResult loadMore(@RequestBody ArticleHomeDto dto) {
+        return null;
+    }
+
+    @PostMapping("/loadnew")
+    public ResponseResult loadNew(@RequestBody ArticleHomeDto dto) {
+        return null;
+    }
+}
