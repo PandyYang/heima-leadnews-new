b30bcd9aec1e6c59d938b0ab4f51530bef32ccf7
PandyYang <yangpandy@gmail.com>
2022-12-15 20:52:05 +0800

    add: 分布式任务调度demo

---
+++ heima-leadnews-test/xxljob-demo/src/main/java/com/heima/xxljob/XxlJonApplication.java
@@ -0,0 +1,13 @@
+package com.heima.xxljob;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJonApplication.class, args);
+    }
+}
