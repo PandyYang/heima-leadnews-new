6a2550b0356822f87472bbc20cc37db4e6039bbc
PandyYang <yangpandy@gmail.com>
2022-12-23 19:54:34 +0800

    add: KafkaStream 热点数据实时计算

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -5,6 +5,7 @@ import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.mess.ArticleVisitStreamMess;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
@@ -35,4 +36,9 @@ public interface ApArticleService extends IService<ApArticle> {
      */
     public ResponseResult load2(ArticleHomeDto dto,Short type,boolean firstPage);
 
+    /**
+     * 更新文章的分值  同时更新缓存中的热点文章数据
+     * @param mess
+     */
+    public void updateScore(ArticleVisitStreamMess mess);
 }
